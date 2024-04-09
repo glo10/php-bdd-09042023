aa5bf0b51858c433c2b7412be6512b064a3cbbfc
glodie <glodie.tshimini@gmail.com>
2024-04-09 12:17:52 +0200

    feat: add array

---
+++ demo/bases/tableaux/index.php
@@ -0,0 +1,30 @@
+<?php
+
+// Tableau indexé
+// déclaration tableau avec []
+$glodie = [
+    'Glodie',
+    'Tshimini',
+    32,
+    75014,
+    'Paris',
+    'France',
+    true,
+    1.69
+];
+
+// déclaration tableau avec la fonction array()
+$maria = array(
+    'Maria',
+    'Doe',
+    42,
+    75004,
+    'Paris',
+    'France',
+    false,
+    1.89
+);
+
+// affichage de l'age
+
+echo $glodie[2];
