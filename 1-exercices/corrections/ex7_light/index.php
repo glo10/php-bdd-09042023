3ae8ef88559d2589ddbd666c05e8c7544e219889
glodie <glodie.tshimini@gmail.com>
2024-04-12 21:57:30 +0200

    feat: correction of exercise 7 + bonus

--- 1-exercices/corrections/ex7_light/index.php
+++ 1-exercices/corrections/ex7_light/index.php
@@ -1,41 +1,49 @@
 <?php
 
+var_dump($_POST, $_FILES);
 /**
- * Upload d'une image
- *
- * @param array $data info sur le fichier
- * @param string $filename nom du fichier
+ * @param array $data 
+ * @param string $uniquePrefix 
  * @return bool
  */
-function downloadImg(array $data, string $filename) : bool
+/**
+ * Upload d'une image
+ * PS : on peut être plus précis en renvoyant une erreur selon un scénario spécifique (trop lourd ou mauvaise extension)
+ *
+ * @param array $data info sur le fichier
+ * @param string $uniquePrefix prefix à ajouter au nom originel du fichier chargé par l'utilisateur
+ * @param integer $maxSize taille max du fichier à ne pas dépasser
+ * @param string $defaultDir dossier de stockage des images
+ * @return boolean
+ */
+function downloadImg(array $data, string $uniquePrefix, int $maxSize = 2000000, $defaultDir = 'upload') : bool
 {
     $pic = $data['tmp_name'];
+    $filename = $uniquePrefix.$data['name'];// Concaténation du prefix avec le nom originel du fichier
     // si $file['tmp_name'] existe alors $tmp vaudra sa valeur sinon $tmp vaut null
     $tmp = $pic ?? null; // idem que $tmp = isset($pic)?$pic:null
-    if($tmp === null) {
+    if($tmp === null) {// pas de fichier temporaire stocké dans un dossier temporaire du serveur
         return false;
     }
+
     // Récupération de l'extension du fichier déposé
     $extension = pathinfo($data['name'], PATHINFO_EXTENSION);
-    // Fonction pour vérifier une expression régulière (permet de vérifier que la donnée respecte bien un motif qu'on a défini)
-    return (preg_match('@jpeg|jpg|png@i', $extension) === 1) && move_uploaded_file($data['tmp_name'], $filename);
+    /**
+     * Fonction preg_match pour vérifier une expression régulière (permet de vérifier que la donnée respecte bien un motif qu'on a défini)
+     * Fonction in_array vérifie qu'une valeur est présente dans un tableau en retournant true ou false
+     * Fonction move_uploaded_file déplace un fichier d'un emplacement à un autre
+     * Il faut bien créer le dossier upload au préalable et idéalement il faudrait vérifier dans le code que ce dossier existe
+     */
+    
+    // vérification de 4 conditions (bonne extension + bon type de contenu + bonne taille + déplacement effectué correctement)
+    return (preg_match('@jpeg|jpg|png@i', $extension) === 1) && 
+    in_array($data['type'], ['image/jpeg', 'image/jpg','image/png', 'image/gif']) &&
+    $data['size'] <= $maxSize &&
+    move_uploaded_file($data['tmp_name'], $defaultDir.'/'.$filename);
 }
 
-var_dump($_POST, $_FILES);
-
-// $_FILES stocke les informations sur les fichiers qui seront déposées
-if(isset($_FILES['picture'])) {
-    // move_uploaded_file déplace un fichier d'un emplacement A vers un emplacement B
-    // $_FILES['picture']['tmp_name'] stocke l'emplacement temporaire du fichier chargé
-    // $_FILES['picture']['name']) stocke le nom initial du fichier
-    if(move_uploaded_file(
-        $_FILES['picture']['tmp_name'],
-        'images/'.$_FILES['picture']['name'])
-    ) {// déplacement ok du point A vers le point B
-        echo 'Chargement du fichier ok';
-    } else {
-        echo 'Chargment du fichier ko';
-    }
+if(isset($_FILES['picture'])) {// appel de la fonction downloadImg uniquement la clé existe = uniquement lors de la soumission du formulaire
+    echo downloadImg($_FILES['picture'], uniqid('img_')) ?'Chargement du fichier ok':'Chargment du fichier ko';
 }
 
 ?>
@@ -47,11 +55,9 @@ if(isset($_FILES['picture'])) {
     <title>Document</title>
 </head>
 <body>
+    <!-- Attention attribut enctype="multipart/form-data" obligatoire sinon impossible de récupérer les infos sur les fichiers côté Back -->
     <form action="" method="post" enctype="multipart/form-data">
-        <input type="text" name="firstname">
-        <input type="text" name="lastname">
         <input type="file" name="picture">
-        <input type="color" name="color">
         <input type="submit" value="Valider">
     </form>
 </body>
